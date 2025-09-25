320a4f1efce4cf449a752f78e97b471a77748e15
DiegoSS14 <sousadiego533@gmail.com>
2025-09-25 12:11:56 -0300

    Adicionando cors como dependência

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import 'express-async-errors'
+import cors from 'cors'
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.get('/', (request, response) => {
     return response.status(200).json({message: 'Olá Dev!'})
