1d3057c6e6b7692ac28f37878e4dc9b63466ff55
DiegoSS14 <sousadiego533@gmail.com>
2025-10-03 10:16:12 -0300

    Config database connection

---
+++ src/shared/http/app.ts
@@ -0,0 +1,30 @@
+import express, { type NextFunction, type Request, type Response } from 'express'
+import swaggerUi from 'swagger-ui-express'
+import 'express-async-errors'
+import cors from 'cors'
+import { AppError } from '../error/AppError.js'
+
+import { routes } from './routes/index.js'
+import swaggerFile from '../../../swagger.json' with {type: "json"} // Importando com type json
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+app.use(routes)
+
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+        return response.status(error.statusCode).json({
+            status: "error",
+            message: error.message
+        })
+    }
+
+    return response.status(500).json({
+        status: "Error",
+        message: "Internal server error"
+    })
+})
+
+export { app }
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,35 +1,13 @@
-import cors from 'cors'
 import 'dotenv/config'
-import express, { type NextFunction, type Request, type Response } from 'express'
-import swaggerUi from 'swagger-ui-express'
-import 'express-async-errors'
-import { AppError } from '../error/AppError.js'
+import { app } from './app.js';
 
-import { routes } from './routes/index.js'
-import swaggerFile from '../../../swagger.json' with {type: "json"} // Importando com type json
+import 'reflect-metadata'
+import { dataSource } from './typeorm/index.js'
 
-const app = express()
-app.use(express.json())
-app.use(cors())
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
-app.use(routes)
-
-app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
-    if (error instanceof AppError) {
-        return response.status(error.statusCode).json({
-            status: "error",
-            message: error.message
-        })
-    }
-
-    return response.status(500).json({
-        status: "Error",
-        message: "Internal server error"
+dataSource.initialize().then(() => {
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+        console.log(`Application is running on port ${port}`)
     })
 })
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-    console.log(`Application is running on port ${port}`)
-})
