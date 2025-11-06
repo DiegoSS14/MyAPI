3b573eb47644b8c2a2c87251eadab0e55b5f0fc4
DiegoSS14 <sousadiego533@gmail.com>
2025-11-06 16:00:28 -0300

    Create migration CreateRefreshTokensTable

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -4,12 +4,16 @@ import { Role } from '../../roles/entities/Role.js'
 import { CreateUsersTable1761179522807 } from './migrations/1761179522807-CreateUsersTable.js'
 import { AddRoleIdToUsersTable1761263808611 } from './migrations/1761263808611-AddRoleIdToUsersTable.js'
 import { User } from '../../users/entities/User.js'
+import { CreateRefreshTokensTable1762454496318 } from './migrations/1762454496318-CreateRefreshTokensTable.js'
 
 export const dataSource = new DataSource({
     type: 'sqlite',
     database: './db.sqlite',
     entities: [Role, User],
     migrations: [
-        CreateRolesTable1759499307163, CreateUsersTable1761179522807, AddRoleIdToUsersTable1761263808611
+        CreateRolesTable1759499307163,
+        CreateUsersTable1761179522807,
+        AddRoleIdToUsersTable1761263808611,
+        CreateRefreshTokensTable1762454496318
     ],
 })
